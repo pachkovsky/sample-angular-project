8ca9e524e737604b65aafb9a8d433db1b0db9b57
Pavel Pachkovskij <pavel.pachkovskij@gmail.com>
2016-01-22 16:24:39 +0300

    add validation on hours

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -8,4 +8,6 @@ class Entry
   field :date, type: Date
 
   validates :note, :hours, :date, :user, presence: true
+
+  validates :hours, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 24, allow_blank: true}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User
   has_many :entries, dependent: :destroy
   has_and_belongs_to_many :managed_users, class_name: 'User', inverse_of: nil
 
-  validates :preferred_working_hours_per_day, numericality: {greater_than_or_equal_to: 0, allow_blank: true}
+  validates :preferred_working_hours_per_day, numericality: {greater_than_or_equal_to: 0, less_than_or_equal_to: 24, allow_blank: true}
   validates :role, presence: true
 
   enumerize :role, in: %w(regular manager admin), predicates: true
